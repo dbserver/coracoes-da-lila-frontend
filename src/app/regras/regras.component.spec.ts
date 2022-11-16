5584aefd66c9c765f4d75b590d47a31a4694c3ca
amandahammes <82393453+amandahammes@users.noreply.github.com>
2022-11-16 16:20:43 -0300

    criação componente regras

---
+++ src/app/regras/regras.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RegrasComponent } from './regras.component';
+
+describe('RegrasComponent', () => {
+  let component: RegrasComponent;
+  let fixture: ComponentFixture<RegrasComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RegrasComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RegrasComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/regras/regras.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-regras',
+  templateUrl: './regras.component.html',
+  styleUrls: ['./regras.component.scss']
+})
+export class RegrasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
