b23aecfb4193bbee6df5117a1d0817f8d24317f7
Rayane Paiva <rayanepaiiva@live.com>
2022-11-22 16:30:47 -0300

    fix US068: correção do nome da variavel

--- src/app/mesa/carta-inicio/carta-inicio.component.ts
+++ src/app/mesa/carta-inicio/carta-inicio.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carta-inicio.component.scss']
 })
 export class CartaInicioComponent implements OnInit {
-  longText = {
+
+  cartaInicio = {
     id:'1',
     nome: 'carta1',
     descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis nisi egestas, porttitor metus vitae, sollicitudin nunc. In euismod felis sit amet lacinia lacinia.'
