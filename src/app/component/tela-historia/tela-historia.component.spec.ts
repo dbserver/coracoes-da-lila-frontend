2c771b034fba0d550e84c34ee670695209271321
quaresmina <arielquaresma90@gmail.com>
2022-11-22 12:42:19 -0300

    incluindo alguns testes

--- src/app/component/tela-historia/tela-historia.component.spec.ts
+++ src/app/component/tela-historia/tela-historia.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { AcessibilidadeComponent } from 'src/app/acessibilidade/acessibilidade.component';
 import { AppComponent } from 'src/app/app.component';
+import { PainelVoltarComponent } from 'src/app/painel-voltar/painel-voltar.component';
 
 import { TelaHistoriaComponent } from './tela-historia.component';
 
@@ -25,11 +27,53 @@ fdescribe('TelaHistoriaComponent', () => {
 
   describe('Validar apresentação do conteúdo da página História do Jogo', () => {
     it('deve conter o ícone de contraste de tela', () => {
-      let appComponent = TestBed.createComponent(AcessibilidadeComponent);
-      let primeiroParagrafo = fixture.nativeElement.querySelector('#switch');
-      let conteudoHistoriaDoJogo = fixture.nativeElement;
-  
-      console.log(primeiroParagrafo);
+      let acessibilidadeComponent = TestBed.createComponent(AcessibilidadeComponent);
+      let contraste = acessibilidadeComponent.nativeElement.querySelector('#contraste');
+     
+      expect(contraste.innerHTML).toContain('Contraste');
+    })
+
+    it('deve conter o ícone de tamanho da fonte', () => {
+      let acessibilidadeComponent = TestBed.createComponent(AcessibilidadeComponent);
+      let fonte = acessibilidadeComponent.nativeElement.querySelector('#icone-fonte');
+     
+      expect(fonte.innerHTML).toContain('A +');
+    })
+
+    it('deve conter o componente painel-voltar com botão e logo', () => {
+      let telaHistoria = fixture.nativeElement.innerHTML;
+    
+      expect(telaHistoria).toContain('<app-painel-voltar');
+    })
+
+    it('deve conter o texto da história do jogo conforme o protótipo', () => {
+      
+      let bodyTelaHistoria = fixture.nativeElement.innerHTML;
+      expect(bodyTelaHistoria).toContain('já era forte na DB');
+      expect(bodyTelaHistoria).toContain('As premissas que orientaram a criação do Corações da Lila foram:');
+      expect(bodyTelaHistoria).toContain('O jogo físico foi apresentado no GUTS-RS, Tecnopuc Experience, AgileBrazil em Belo Horizonte e TDC');
+    })
+
+    it('deve conter as imagens com texto alternativo da página igual ao protótipo', () => {
+      
+      let imagens = fixture.nativeElement.querySelectorAll('img');
+
+      expect(imagens[0].alt).toEqual('Caixas de vários jogos sobre uma mesa, com cartazes relacionando as particularidades de cada um com o trabalho, por exemplo criatividade, cooperação, priorização, etc.');
+      expect(imagens[1].alt).toEqual('Protótipo com papeis coloridos recortados à mão e anotações semi-ilegíveis que deviam fazer sentido na época')
+      expect(imagens[2].alt).toEqual('Quatro mulheres brancas sorriem e mostram cópias do manual e das cartas do jogo, que também está disposto numa mesinha de vidro entre elas.')
     })
   })
+
+  it('deve conter o ícone de contraste de tela', () => {
+    let appComponent = TestBed.createComponent(AcessibilidadeComponent);
+    let contrasteBotao = appComponent.nativeElement.querySelector('#switch');
+    contrasteBotao.click();
+    appComponent.detectChanges();
+
+    let corpoDoTexto = fixture.debugElement.query(By.css('body'));
+    console.log(corpoDoTexto);
+    console.log(fixture.nativeElement);
+    console.log(contrasteBotao.checked);
+  })
+
 });
