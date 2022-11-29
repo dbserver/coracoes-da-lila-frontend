ac76f74ba651543584c9c1da4e38a63c46068cb3
Rayane Paiva <rayanepaiiva@live.com>
2022-11-29 14:04:24 -0300

    fix: resolvido erro de import

--- src/app/service/mesa.service.spec.ts
+++ src/app/service/mesa.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { MesaService } from './mesa.service';
@@ -6,7 +7,9 @@ describe('MesaService', () => {
   let service: MesaService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(MesaService);
   });
 
