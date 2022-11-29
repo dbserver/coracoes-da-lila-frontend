e8f0a7cf1bcea864267de9016342b0a673ad7551
Rayane Paiva <rayanepaiiva@live.com>
2022-11-29 13:58:04 -0300

    fix: focused describe removido

--- src/app/component/painel-voltar/painel-voltar.component.spec.ts
+++ src/app/component/painel-voltar/painel-voltar.component.spec.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 
 import { PainelVoltarComponent } from './painel-voltar.component';
 
-fdescribe('PainelVoltarComponent', () => {
+describe('PainelVoltarComponent', () => {
   let component: PainelVoltarComponent;
   let fixture: ComponentFixture<PainelVoltarComponent>;
 
