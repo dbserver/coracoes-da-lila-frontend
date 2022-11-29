335a77b7d22ec22bf8ad49ccc3a643dbff2c40a5
Rayane Paiva <rayanepaiiva@live.com>
2022-11-29 13:20:02 -0300

    fix: resolvido erro de import

--- src/app/montar-cartas/montar-cartas.component.spec.ts
+++ src/app/montar-cartas/montar-cartas.component.spec.ts
@@ -1,14 +1,16 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MontarCartasComponent } from './montar-cartas.component';
 
-describe('MontarCartasComponent', () => {
+fdescribe('MontarCartasComponent', () => {
   let component: MontarCartasComponent;
   let fixture: ComponentFixture<MontarCartasComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MontarCartasComponent ]
+      declarations: [ MontarCartasComponent ],
+      imports: [HttpClientTestingModule]
     })
     .compileComponents();
   });
