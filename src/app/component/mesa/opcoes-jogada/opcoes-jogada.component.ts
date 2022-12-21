6c7d23e9b3d7d99684f72bbf9c46598678c13ba2
Kalebe Nascimento <kalebe.nascimento19@gmail.com>
2022-12-21 12:43:45 -0300

    Adicionadas as requisições para receber informações dos jogadores
    Co-authored-by: Luisa <luisasasilva19@gmail.com>

--- src/app/component/mesa/opcoes-jogada/opcoes-jogada.component.ts
+++ src/app/component/mesa/opcoes-jogada/opcoes-jogada.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { CartaDoJogo } from 'src/app/model/cartaDoJogo';
+import { Jogador } from 'src/app/model/jogador';
+import { Sala } from 'src/app/model/sala';
+import { AreaDeCompraService } from 'src/app/service/area-de-compra-service/area-de-compra.service';
+import { MesaJogoService } from 'src/app/service/mesa-jogo-service/mesa-jogo.service';
 
 @Component({
   selector: 'app-opcoes-jogada',
@@ -6,10 +11,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./opcoes-jogada.component.scss']
 })
 export class OpcoesJogadaComponent implements OnInit {
+  public sala: Sala = {} as Sala;
+  public jogador: Jogador = {} as Jogador;
+  public listacartasMao: Array<CartaDoJogo> = [];
 
-  constructor() { }
+  constructor(
+    private mesaJogoService: MesaJogoService,
+    private areaCompraService: AreaDeCompraService
+  ) {}
 
   ngOnInit(): void {
+    this.mesaJogoService.getemitJogadorObservable().subscribe((jogador) => {
+      this.mesaJogoService.getemitSalaObservable().subscribe((sala) => {
+        this.sala = sala;
+        this.jogador = jogador;
+        this.escutaEventoCompra()
+        this.mesaJogoService.setJogadorAtualNaMesa(this.jogador);
+        console.log(this.jogador);
+      });
+    });
   }
 
+  public escutaEventoCompra(): void {
+    this.areaCompraService.emitirCartaJogo.subscribe(
+      (listacartasMao: CartaDoJogo[]) =>
+        (this.jogador.cartasDoJogo = listacartasMao)
+    );
+    this.mesaJogoService.getemitSalaObservable().subscribe((sala) => {
+      this.jogador = this.sala.jogadores?.find(
+        (jogador) => jogador.id == this.jogador?.id
+      ) as Jogador;
+    });
+  }
 }
