e56b1c8e75de939a371169ba696a43a39bb0dbaf
Júlio Machado <julioapm@gmail.com>
2022-12-26 17:49:42 -0300

    HotFix variáveis de ambiente para main

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  API_URL:'https://dev-backendlila.onrender.com/',
+  CLIENT_URL: 'https://dev-frontendlila.onrender.com/',
+  WEBSOCKET_URL: 'ws://dev-backendlila.onrender.com/'
+};
