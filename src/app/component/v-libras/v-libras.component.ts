1ac0584b51985bf45bc117ab245b0d987b9969c7
Kal Nascimento <kalebe.nascimento19@gmail.com>
2022-12-30 17:59:24 -0300

    Ajustes finais

--- src/app/component/v-libras/v-libras.component.ts
+++ src/app/component/v-libras/v-libras.component.ts
@@ -7,7 +7,6 @@ import { NavigationEnd, Router } from '@angular/router';
   styleUrls: ['./v-libras.component.scss']
 })
 export class VLibrasComponent implements OnInit {
-  public render: boolean = true;
   public href: string = "";
 
   constructor(private router: Router) {}
@@ -16,9 +15,6 @@ export class VLibrasComponent implements OnInit {
     this.router.events.subscribe(route => {
       if (route instanceof NavigationEnd) {
         this.href = route.url
-        console.log(this.href)
-        console.log(this.renderizarVLibras())
-        this.render = this.renderizarVLibras();
       }
     })
   }
