8cfb71fdea8122e734040230918242e82ff9a79b
quaresmina <arielquaresma90@gmail.com>
2023-01-11 17:10:56 -0300

    test: configurando providers do arquivo

--- src/app/component/mesa/seleciona-categoria/seleciona-categoria.component.spec.ts
+++ src/app/component/mesa/seleciona-categoria/seleciona-categoria.component.spec.ts
@@ -1,14 +1,21 @@
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
 
 import { SelecionaCategoriaComponent } from './seleciona-categoria.component';
 
 describe('SelecionaCategoriaComponent', () => {
   let component: SelecionaCategoriaComponent;
   let fixture: ComponentFixture<SelecionaCategoriaComponent>;
+  const hash = 'hash';
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SelecionaCategoriaComponent ]
+      declarations: [ SelecionaCategoriaComponent ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [HttpClient, HttpHandler, 
+        { provide: ActivatedRoute, useValue: { snapshot: { paramMap: convertToParamMap({ 'hash': hash }) } } }]
     })
     .compileComponents();
 
