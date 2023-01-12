aae0481992ede47ec15f68012035ac9006bb7d28
quaresmina <arielquaresma90@gmail.com>
2023-01-12 19:55:44 -0300

    test: configurações de providers e import para testes

--- src/app/component/montar-cartas/montar-cartas.component.spec.ts
+++ src/app/component/montar-cartas/montar-cartas.component.spec.ts
@@ -1,5 +1,9 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/compiler';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { ModalZoomComponent } from '../mesa/modal-zoom/modal-zoom.component';
 
 import { MontarCartasComponent } from './montar-cartas.component';
 
@@ -10,7 +14,12 @@ describe('MontarCartasComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ MontarCartasComponent ],
-      imports: [HttpClientTestingModule]
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [HttpClientTestingModule, MatIconModule],
+      providers: [ModalZoomComponent,
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: MatDialog, useValue: {} },
+      ]
     })
     .compileComponents();
   });
