92ef89c0ec322e935ed1610b33a0781d829f6587
diegopereira99 <53828569+diegopereira99@users.noreply.github.com>
2023-03-03 17:27:46 -0300

    Azure pipelines (#5)
    * removing comments from azure-pipelines
    
    * printing pipeline variables
    
    * printing pipeline variables
    
    * printing pipeline variables
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * fixing pipe conditions
    
    * adding k8s config
    
    * adding k8s ingress
    
    * changing enviroments
    
    ---------
    
    Co-authored-by: Diego Marcelo <diego.marcelo@db.tec.br>
    Co-authored-by: Ivo Fulco <53705736+ivofulco@users.noreply.github.com>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL:'https://main-backendlila.onrender.com/',
-  CLIENT_URL: 'https://main-frontendlila.onrender.com/',
-  WEBSOCKET_URL: 'ws://main-backendlila.onrender.com/'
+  API_URL:'https://coracoesdalila-api-dev.paas.dbserver.com.br',
+  CLIENT_URL: 'https://coracoesdalila-dev.paas.dbserver.com.br/',
+  WEBSOCKET_URL: 'ws://coracoesdalila-api-dev.paas.dbserver.com.br'
 };
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL:'https://dev-backendlila.onrender.com/',
-  CLIENT_URL: 'https://dev-frontendlila.onrender.com/',
-  WEBSOCKET_URL: 'ws://dev-backendlila.onrender.com/'
+  API_URL:'https://coracoesdalila-api-dev.paas.dbserver.com.br',
+  CLIENT_URL: 'https://coracoesdalila-dev.paas.dbserver.com.br/',
+  WEBSOCKET_URL: 'ws://coracoesdalila-api-dev.paas.dbserver.com.br'
 };
