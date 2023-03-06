d7880a1d1d9568669a5611559dd5910412c05b41
Diego Marcelo <diego.marcelo@db.tec.br>
2023-03-06 14:32:13 -0300

    changing enviroments

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL:'https://coracoesdalila-api-dev.paas.dbserver.com.br/',
+  API_URL:'https://coracoesdalila-dev-api.paas.dbserver.com.br/',
   CLIENT_URL: 'https://coracoesdalila-dev.paas.dbserver.com.br/',
-  WEBSOCKET_URL: 'ws://coracoesdalila-api-dev.paas.dbserver.com.br/'
+  WEBSOCKET_URL: 'ws://coracoesdalila-dev-api.paas.dbserver.com.br/'
 };
